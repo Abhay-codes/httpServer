05c594006011be17e2b1df1dd371e625279a70af
Abhay-codes <abhaysingh002001@gmail.com>
2023-10-25 20:01:38 +0530

    added a log file for the requasts

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const http =require('http') //to create a server 
-const myServer=http.createServer((req,res)=>{; //create a server  //callback function 
+const fs=require("fs");
+//console.log(Date.now());
+const myServer=http.createServer((req,res)=>{//create a server  //callback function 
 //req --> info about  requasting user 
 // res 
-    console.log("new req recieved");
-    res.end("Hello from server")
-}
-);
+    
+    const log = `${Date.now()}: ${req.url} New requast received\n`;
+
+    fs.appendFile('log.txt',log,(err,data)=>{
+        res.end("Hello from server");
+
+
+    });
+    
+    
+});
 myServer.listen(8000,() => console.log("server started "));
  //port to run the server 
-// multiple servers cant run on same port 
+// multiple servers cant run on same port start 

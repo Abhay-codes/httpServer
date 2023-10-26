28c7590f1ffc26cd4ea613747ac61ef260306426
Abhay-codes <abhaysingh002001@gmail.com>
2023-10-26 22:30:00 +0530

    used url and then express

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-const http =require('http') //to create a server 
-const fs=require("fs");
+//const http =require('http') //to create a server 
+//const fs=require("fs");
+//const url =require("url")
+const express=require("express"); 
+const app =express();//handler function , different http methods
 //console.log(Date.now());
-const myServer=http.createServer((req,res)=>{//create a server  //callback function 
+
+app.get("/",(req,res)=>{
+    return res.send("hllo from the homepage ");
+});
+app.get("/about",(req,res)=>{
+    return res.send("hello  " + req.query.name);
+});
+app.post("/about",(req,res)=>{
+    return res.send("post req from about");
+});
+
+//const myServer=http.createServer(app);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const myServer=http.createServer((req,res)=>{//create a server  //callback function 
 //req --> info about  requasting user 
 // res 
     
-    const log = `${Date.now()}: ${req.url} New requast received\n`;
+    const log = `${Date.now()}: 
+        
+     ${req.url} New requast received\n`;
 
     fs.appendFile('log.txt',log,(err,data)=>{
         switch(req.url){
@@ -25,7 +58,11 @@ const myServer=http.createServer((req,res)=>{//create a server  //callback funct
     });
     
     
-});
-myServer.listen(8000,() => console.log("server started "));
+});*/
+
+
+//myServer.listen(8000,() => console.log("server started "));
  //port to run the server 
 // multiple servers cant run on same port start 
+//alternative using express 
+app.listen(8000,()=>console.log("SERVER STARTED!"));
